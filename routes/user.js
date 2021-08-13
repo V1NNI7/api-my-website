ee42b1479bab1a0ff54b6dcd653e08a8cb5bb3fe
v1nni7 <vinicezar800@hotmail.com>
2021-08-13 12:48:01 -0300

    Mudanças na roda de post

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { Users } = require('../models');
 const { Op } = require('sequelize');
 const { sha256 } = require('js-sha256');
+const mongoose = require('mongoose');
 
-router.post('/', async (req, res) => {
+/* router.post('/', async (req, res) => {
     const response = await Users.create({
         name: req.body.name,
         email: req.body.email,
@@ -12,7 +13,49 @@ router.post('/', async (req, res) => {
         password: sha256(req.body.password + '$@#324'),
     });
     res.status(200).json(response)
-});
+}); */
+
+router.post('/', (req, res) => {
+    var erros = []
+
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+        errors.push({texto: "Texto inválido"})
+    }
+
+    if(!req.body.username || typeof req.body.username == undefined || req.body.username == null){
+        errors.push({texto: "Usuário inválido"})
+    }
+
+    if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
+        errors.push({texto: "Email inválido"})
+    }
+
+    if(!req.body.password || typeof req.body.password == undefined || req.body.password == null){
+        errors.push({texto: "Senha inválido"})
+    }
+
+    if(req.body.password.length < 4) {
+        erros.push({texto: "Senha muito curta"})
+    }
+
+    if(erros.length > 0) {
+        console.log({erros: erros})
+    } else {
+        Users.findOne({email: req.body.email}).then((users) => {
+            if(users) {
+                req.flash("error_msg", "Já existe uma conta com este email no nosso sistema!")
+            } else {
+                const response = new Users({
+                    name: req.body.name,
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                })
+            }
+        })
+    }
+})
+
 
 router.get('/', async (req, res) => {
     const response = await Users.findAll({
@@ -21,7 +64,7 @@ router.get('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-    const response = await Users.update(req.body, {
+    await Users.update(req.body, {
         where: {
             id: req.params.id
         }
@@ -30,7 +73,7 @@ router.put('/:id', async (req, res) => {
 })
 
 router.delete('/:id', async (req, res) => {
-    const response = await Users.destroy({
+    await Users.destroy({
         where: {
             id: req.params.id
         }
