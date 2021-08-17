2459a5d574abfaa4ec911331c1240a70ad92bbed
v1nni7 <vinicezar800@hotmail.com>
2021-08-17 09:34:56 -0300

    Verificando uso do email no Banco de Dados

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { Users } = require('../models');
 const { Op } = require('sequelize');
 const { sha256 } = require('js-sha256');
-const mongoose = require('mongoose');
 
 /* router.post('/', async (req, res) => {
     const response = await Users.create({
@@ -15,47 +14,24 @@ const mongoose = require('mongoose');
     res.status(200).json(response)
 }); */
 
-router.post('/', (req, res) => {
-    var erros = []
-
-    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
-        errors.push({texto: "Texto inválido"})
-    }
-
-    if(!req.body.username || typeof req.body.username == undefined || req.body.username == null){
-        errors.push({texto: "Usuário inválido"})
-    }
-
-    if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
-        errors.push({texto: "Email inválido"})
-    }
-
-    if(!req.body.password || typeof req.body.password == undefined || req.body.password == null){
-        errors.push({texto: "Senha inválido"})
-    }
-
-    if(req.body.password.length < 4) {
-        erros.push({texto: "Senha muito curta"})
-    }
-
-    if(erros.length > 0) {
-        console.log({erros: erros})
+router.post('/', async (req, res) => {
+    const result = await Users.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+    if(result === null) {
+        const response = await Users.create({
+           name: req.body.name,
+           email: req.body.email,
+           username: req.body.username,
+           password: sha256(req.body.password + '$@#324'),
+        });
+        res.status(200).json(response);
     } else {
-        Users.findOne({email: req.body.email}).then((users) => {
-            if(users) {
-                req.flash("error_msg", "Já existe uma conta com este email no nosso sistema!")
-            } else {
-                const response = new Users({
-                    name: req.body.name,
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password,
-                })
-            }
-        })
+        res.status(200).json('Email já existente.')
     }
-})
-
+});
 
 router.get('/', async (req, res) => {
     const response = await Users.findAll({
